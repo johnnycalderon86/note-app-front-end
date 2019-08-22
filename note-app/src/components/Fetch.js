18d718c30fc5e8f32696b531261130555480f5c9
Johnny <johnnycalderondeveloper@gmail.com>
2019-08-22 14:12:09 +0200

    created fetch component

---
+++ note-app/src/components/Fetch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Fetch() {
+  return (
+    <div className="Fetch">
+       
+    </div>
+  );
+}
+
+export default Fetch;
---
+++ note-app/src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function List() {
+  return (
+    <div className="List">
+        <h1>Notes</h1>
+            <div>
+                <button>List of Notes</button>
+            </div>
+    </div>
+  );
+}
+
+export default List;
